50a08e125a23aeba8bde80a0897a6b35bcf6401b
Rob <rob@Robs-MacBook-Pro.local>
2020-01-31 18:37:52 -0500

    Added test to make sure numbers passed are variables.

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+// Random number function
 function randomNumber(bottom, top) {
+    if (isNaN(bottom) === false || isNaN(top) === false) {
+        // throw new Error('Please enter a number.');
+        alert('Please enter a number.');
+    }
     return Math.floor(Math.random() * (top - bottom + 1) + bottom);
-
 }
+
+// User random number inputs.
 
 var userLowNumber = prompt('What is the begining of the random number range?');
 var lowNumber = parseInt(userLowNumber);
